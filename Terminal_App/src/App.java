7a94bb201433f877d8067524f399e9f5f47821d9
DerikOmond <derik.omond@gmail.com>
2022-11-01 19:41:22 +0000

    Imports used
    Imported Scanner to be used when reading user inputs
    
    Imported ArrayList as songs will be defined as being part of an arraylist.
    
    'continue' variable was changed to '_continue' as error occurred. Possibly as 'continue' is already used for iteration in a loop.

--- Terminal_App/src/App.java
+++ Terminal_App/src/App.java
@@ -1,11 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception
     {
+        Scanner sc = new Scanner(System.in);
         do
         {
 
             System.out.print("Continue? ");
-            String continue = sc.nextLine();
+            String _continue = sc.nextLine();
         }
         while();
     }
