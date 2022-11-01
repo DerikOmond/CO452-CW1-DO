d43a5e62042d17904da98314acf6432511be9c35
DerikOmond <derik.omond@gmail.com>
2022-11-01 19:15:03 +0000

    Setting up song constructor
    Song constructor will have string and integer values inputs and assigned to the class attributes.

--- Terminal_App/src/Song.java
+++ Terminal_App/src/Song.java
@@ -3,5 +3,12 @@ public class Song
     String title;
     String artist;
     int playCount;
+
+    public void Song(String title, String artist, int playCount)
+    {
+        this.title = title;
+        this.artist = artist;
+        this.playCount = playCount;
+    }
     
 }
